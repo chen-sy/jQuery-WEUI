cd8248b432bfe5cf5ed7fb2a7cfccfd451ac79f4
chensy <724060052@qq.com>
2018-08-23 00:46:38 +0800

    添加项目文件。

---
+++ jQuery WEUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQuery_WEUI.Startup))]
+namespace jQuery_WEUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
